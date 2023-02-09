eb57694c4135d59d9eb51560f15f20b54af9e377
Henrique Marcel dos Santos <henrique_marcel1@hotmail.com>
2023-02-09 18:00:46 -0300

    loading-promise

---
+++ src/components/Item.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import camisaBranca from '../assets/camisaBranca.png';
+
+const Items = (props) => {
+  return (
+    <div className='flex justify-center'>
+        <div className='p-0 bg-white mt-10'>
+          {/* Image */}
+          <div>
+            <img src={props.image} alt='logo' className='h-60 rounded-xl'/>
+          </div>
+          {/*Title */}
+          <div>
+            <h1 className='font-extrabold mt-5 ml-1 text-lg'>{props.title}</h1>
+          </div>
+          {/* Price */}
+          <div>
+            <p className='font-thin text-xs ml-1 mt-2'>{`A partir de R$ ${props.price}`}</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Items;
